20bccd843374528a52f8c1cb626507034f8eb983
CordHouse <qkqktl5310@gmail.com>
2022-12-05 02:04:07 +0900

    22.12.05

---
+++ src/Pro_KaKao_거리두기_확인하기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Pro_KaKao_거리두기_확인하기 {
+    public static void main(String[] args) {
+        String word = "AAAAE";
+        System.out.println(solution(word));
+    }
+    static List<String> wordList = new LinkedList<>();
+    public static int solution(String word) {
+        int answer = 0;
+        String[] moumWord = {"A","E","I","O","U"};
+        for(int count = 1; count <= moumWord.length; count++){
+            StringBuilder wordMake = new StringBuilder();
+            wordCycle(moumWord, count, wordMake);
+        }
+        Collections.sort(wordList);
+        answer = searchWord(word);
+        return answer;
+    }
+
+    public static void wordCycle(String[] moumWord, int count, StringBuilder wordMake){
+        if(count == 0){
+            wordList.add(wordMake.toString());
+            return;
+        }
+        for(int i = 0; i < moumWord.length; i++){
+            wordMake.append(moumWord[i]);
+            wordCycle(moumWord, count-1, wordMake);
+            wordMake.setLength(wordMake.length()-1);
+        }
+    }
+
+    public static int searchWord(String word){
+        for(int i = 0; i < wordList.size(); i++){
+            if(wordList.get(i).equals(word)){
+                return i+1;
+            }
+        }
+        return 0;
+    }
+
+}
