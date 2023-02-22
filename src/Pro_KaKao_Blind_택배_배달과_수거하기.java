cf653a144c57e9688d8f1a052ce65cf251b0c370
CordHouse <qkqktl5310@gmail.com>
2023-02-23 02:15:04 +0900

    2023.02.23 Pro_KaKao_Blind_택배_배달과_수거하기 (수정)

--- src/Pro_KaKao_Blind_택배_배달과_수거하기.java
+++ src/Pro_KaKao_Blind_택배_배달과_수거하기.java
@@ -9,29 +9,19 @@ public class Pro_KaKao_Blind_택배_배달과_수거하기 {
 
     public static long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-        List<Integer> deliveriesList = intArrayToList(deliveries); // 배달 -> list 변환
-        List<Integer> pickupsList = intArrayToList(pickups); // 수거 -> list 변환
         long d = 0; // 배달 포인트
         long p = 0; // 수거 포인트
 
         for(int i = n-1; i >= 0; i--) {
-            d += deliveriesList.get(i);
-            p += pickupsList.get(i);
+            d += deliveries[i];
+            p += pickups[i];
 
             while(d > 0 || p > 0) {
                 d -= cap;
                 p -= cap;
-                answer += (i+1) * 2;
+                answer += (i+1) * 2L;
             }
         }
         return answer;
-    }
-
-    public static List<Integer> intArrayToList(int[] arr) {
-        List<Integer> tempList = new LinkedList<>();
-        for(int value : arr) {
-            tempList.add(value);
-        }
-        return tempList;
     }
 }
