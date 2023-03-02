56cffa1e306f0224331893e7dff8735b3fdfa577
CordHouse <qkqktl5310@gmail.com>
2023-03-03 00:44:09 +0900

    2023.03.03 Pro_KaKao_Blind_표_병합(54.5)

--- src/Pro_KaKao_Blind_표_병합.java
+++ src/Pro_KaKao_Blind_표_병합.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class Pro_KaKao_Blind_표_병합 {
     private static String[][] table = new String[51][51];
-    private static int[][] tableMerge = new int[51][51];
-    private static int number = 1;
+    private static int[][] tableNumber = new int[51][51];
     private static List<String> ans = new LinkedList<>();
+    private static int number = 1;
 
     public static void main(String[] args) {
         String[] commands = {"UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"};
@@ -11,33 +11,35 @@ public class Pro_KaKao_Blind_표_병합 {
     }
 
     public static String[] solution(String[] commands) {
-        String[] answer = {};
         init();
         for(String command : commands) {
-            String[] commandSplit = command.split(" ");
-            commandStart(commandSplit);
+            startCommand(command.split(" "));
         }
-        answer = ans.toArray(String[]::new);
+        String[] answer = new String[ans.size()];
+        for(int i = 0; i < ans.size(); i++) {
+            answer[i] = ans.get(i);
+        }
         return answer;
     }
 
-    public static void init() {
-        for(int r = 1; r <= 50; r++) {
-            for(int c = 1; c <= 50; c++) {
+    public static void init(){
+        for(int r = 1; r < 51; r++) {
+            for(int c = 1; c < 51; c++) {
                 table[r][c] = "";
-                tableMerge[r][c] = 0;
+                tableNumber[r][c] = 0;
             }
         }
     }
 
-    public static void commandStart(String[] command) {
-        if(command[0].equals("UPDATE")) {
+    public static void startCommand(String[] command) {
+        String cmd = command[0];
+        if(cmd.equals("UPDATE")) {
             update(command);
         }
-        else if(command[0].equals("MERGE")) {
+        else if(cmd.equals("MERGE")) {
             merge(command);
         }
-        else if(command[0].equals("UNMERGE")) {
+        else if(cmd.equals("UNMERGE")) {
             unMerge(command);
         }
         else {
@@ -45,100 +47,105 @@ public class Pro_KaKao_Blind_표_병합 {
         }
     }
 
-    public static void tableCycle(String compare, String value) {
-        for(int r = 1; r <= 50; r++) {
-            for(int c = 1; c <= 50; c++) {
-                if(table[r][c].equals(compare)) {
-                    table[r][c] = value;
-                }
-            }
-        }
-    }
-
-    public static void tableMergeCycle(int compare, String tableValue, int tableMergeValue) {
-        for(int r = 1; r <= 50; r++) {
-            for(int c = 1; c <= 50; c++) {
-                if(tableMerge[r][c] == compare) {
-                    table[r][c] = tableValue;
-                    tableMerge[r][c] = tableMergeValue;
-                }
-            }
-        }
-    }
-
     public static void update(String[] command) {
+        // 위치를 지정해서 값 넣기
         if(command.length == 4) {
             int r1 = Integer.parseInt(command[1]), c1 = Integer.parseInt(command[2]);
-            if(tableMerge[r1][c1] != 0 && !table[r1][c1].equals("")) {
-                tableCycle(table[r1][c1], command[3]);
+            if(tableNumber[r1][c1] == 0) {
+                table[r1][c1] = command[3];
                 return;
             }
-            table[r1][c1] = command[3];
+            for(int r = 1; r < 51; r++) {
+                for(int c = 1; c < 51; c++) {
+                    if(tableNumber[r][c] == tableNumber[r1][c1]) {
+                        table[r][c] = command[3];
+                    }
+                }
+            }
             return;
         }
-        tableCycle(command[1], command[2]);
+        for(int r = 1; r < 51; r++) {
+            for(int c = 1; c < 51; c++) {
+                if(table[r][c].equals(command[1])) {
+                    table[r][c] = command[2];
+                }
+            }
+        }
     }
 
     public static void merge(String[] command) {
         int r1 = Integer.parseInt(command[1]), c1 = Integer.parseInt(command[2]);
         int r2 = Integer.parseInt(command[3]), c2 = Integer.parseInt(command[4]);
-        if(r1 == r2 && c1 == c2) {
-            return;
+        int min = tableNumber[r2][c2];
+        int max = tableNumber[r1][c1];
+
+        // 선택한 두 위치의 셀이 같은 값일 경우 무시
+        if(tableNumber[r1][c1] == tableNumber[r2][c2] && tableNumber[r1][c1] != 0 && tableNumber[r2][c2] != 0) {
+            return ;
         }
-        if(tableMerge[r1][c1] != 0 || tableMerge[r2][c2] != 0){
-            if(table[r1][c1].equals("") && !table[r2][c2].equals("")) {
-                tableCycle(table[r1][c1], table[r2][c2]);
-                tableMergeCycle(tableMerge[r1][c1], table[r2][c2], tableMerge[r2][c2]);
+
+        if(table[r1][c1].equals("") && !table[r2][c2].equals("")) {
+            if(tableNumber[r1][c1] == 0 && tableNumber[r2][c2] == 0) {
+                set(r1, c1, r2, c2);
                 return;
             }
-            if(tableMerge[r1][c1] != 0 && tableMerge[r2][c2] != 0 && table[r1][c1].equals("")){
+            if(min == 0) {
                 table[r1][c1] = table[r2][c2];
-                tableMerge[r1][c1] = tableMerge[r2][c2];
+                tableNumber[r1][c1] = tableNumber[r2][c2];
                 return;
             }
-            else if(tableMerge[r1][c1] != 0 && tableMerge[r2][c2] != 0 && table[r2][c2].equals("")){
+            cycle(min, max, table[r2][c2]);
+        }
+        else {
+            if(tableNumber[r1][c1] == 0 && tableNumber[r2][c2] == 0) {
+                set(r2, c2, r1, c1);
+                return;
+            }
+            if(min == 0) {
                 table[r2][c2] = table[r1][c1];
-                tableMerge[r2][c2] = tableMerge[r1][c1];
+                tableNumber[r2][c2] = tableNumber[r1][c1];
                 return;
             }
-            else if(tableMerge[r1][c1] != 0 && tableMerge[r2][c2] != 0) {
-                tableCycle(table[r2][c2], table[r1][c1]);
-                tableMergeCycle(tableMerge[r2][c2], table[r1][c1], tableMerge[r1][c1]);
-                return;
-            }
-            else if(tableMerge[r1][c1] != 0) {
-                table[r2][c2] = table[r1][c1];
-                tableMerge[r2][c2] = tableMerge[r1][c1];
-                return;
-            }
-            else if(tableMerge[r2][c2] != 0) {
-                table[r1][c1] = table[r2][c2];
-                tableMerge[r1][c1] = tableMerge[r2][c2];
-                return;
+            cycle(min, max, table[r1][c1]);
+        }
+    }
+
+    public static void set(int r1, int c1, int r2, int c2) {
+        tableNumber[r1][c1] = number;
+        tableNumber[r2][c2] = number;
+        table[r1][c1] = table[r2][c2];
+        number ++;
+    }
+
+    public static void cycle(int min, int max, String value) {
+        for(int r = 1; r < 51; r++) {
+            for(int c = 1; c < 51; c++) {
+                if(tableNumber[r][c] == min) {
+                    table[r][c] = value;
+                    tableNumber[r][c] = max;
+                }
             }
         }
-        tableMerge[r1][c1] = number;
-        tableMerge[r2][c2] = number;
-        number++;
-        table[r2][c2] = table[r1][c1];
     }
 
     public static void unMerge(String[] command) {
         int r1 = Integer.parseInt(command[1]), c1 = Integer.parseInt(command[2]);
-        int temp = tableMerge[r1][c1];
-        int tempNumber = tableMerge[r1][c1];
-        String tempString = table[r1][c1];
-        tableMergeCycle(temp, "", 0);
-        table[r1][c1] = tempString;
-        tableMerge[r1][c1] = tempNumber;
+        String value = table[r1][c1];
+        if(tableNumber[r1][c1] == 0) {
+            return;
+        }
+        cycle(tableNumber[r1][c1], 0, "");
+        table[r1][c1] = value;
     }
 
     public static void print(String[] command) {
         int r1 = Integer.parseInt(command[1]), c1 = Integer.parseInt(command[2]);
-        if(table[r1][c1].equals("")){
+        if(table[r1][c1].equals("")) {
             ans.add("EMPTY");
             return;
         }
         ans.add(table[r1][c1]);
     }
+
+
 }
